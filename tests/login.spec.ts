7dfd8a6ba4e75834736f7628a0f55d36b5cfeec6
Wescley Martins <wescleymartins02@outlook.com>
2026-08-05 07:36:02 -0300

    login.spec.ts fixed

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 import { LoginPage } from './support/pages/loginPage'
 
 
-test.only('Successfully logs in', async ({ page }) => {
+test('Successfully logs in', async ({ page }) => {
     const loginPage: LoginPage = new LoginPage(page)
     await loginPage.login()
     await expect(page).toHaveTitle('Seu Barriga - Home')
